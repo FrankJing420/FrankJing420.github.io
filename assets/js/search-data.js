55c0eaafaeae3a167c42b2cb9fdceb54b0c2f69e
FrankJing420 <frankjing420@163.com>
2026-04-16 17:13:19 +0000

    Deploying to gh-pages from @ FrankJing420/FrankJing420.github.io@0336a2c0bb7477ab1dc3bdb3621478a0d9915078 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,7 +72,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-a-post-with-plotly-js",
+            },{id: "post-n2n",
+        
+          title: "N2N",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/N2N/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
